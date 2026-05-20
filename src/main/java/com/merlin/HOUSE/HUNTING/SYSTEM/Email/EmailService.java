b357f4776c2ad22029e13143d94a14e437562f49
Lawrence_Mungai <mungailawrence52@gmail.com>
2026-05-20 08:37:06 +0300

    Add emailService

--- src/main/java/com/merlin/HOUSE/HUNTING/SYSTEM/Email/EmailService.java
+++ src/main/java/com/merlin/HOUSE/HUNTING/SYSTEM/Email/EmailService.java
@@ -1,4 +1,31 @@
 package com.merlin.HOUSE.HUNTING.SYSTEM.Email;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String EmailFrom;
+
+    @Async
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(EmailFrom);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+
+    }
 }
--- src/main/java/com/merlin/HOUSE/HUNTING/SYSTEM/HouseHuntingSystemApplication.java
+++ src/main/java/com/merlin/HOUSE/HUNTING/SYSTEM/HouseHuntingSystemApplication.java
@@ -2,8 +2,10 @@ package com.merlin.HOUSE.HUNTING.SYSTEM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class HouseHuntingSystemApplication {
 
 	public static void main(String[] args) {
